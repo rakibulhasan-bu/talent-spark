cd1ba4a27d1ae7547404432f3eded487ff708498
Rakibul Hasan <rakibulhasanbu@gmail.com>
2023-04-11 19:39:08 +0600

    filter system added

--- src/components/AppliedJobs.jsx
+++ src/components/AppliedJobs.jsx
@@ -1,32 +1,86 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { getShoppingCart } from "../../public/utilities/fakedb";
 import { useLoaderData } from "react-router-dom";
 import AppliedSingleJob from "./AppliedSingleJob";
 
 const AppliedJobs = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [filterShow, setFilterShow] = useState(false);
+  const [allJobData, setAllJobData] = useState([]);
+
   const jobData = useLoaderData();
   const storedData = getShoppingCart();
 
-  let jobDataArray = [];
-  for (let id in storedData) {
-    const foundJob = jobData.find(
-      (singleJobData) => singleJobData.job_id === id
-    );
-    if (foundJob) {
-      jobDataArray.push(foundJob);
+  useEffect(() => {
+    let jobDataArray = [];
+    for (let id in storedData) {
+      const foundJob = jobData.find(
+        (singleJobData) => singleJobData.job_id === id
+      );
+      if (foundJob) {
+        jobDataArray.push(foundJob);
+      }
     }
-  }
-  console.log(jobDataArray);
+    setAllJobData(jobDataArray);
+  }, [filterShow]);
+
+  const handleFilterRemote = () => {
+    const remoteJob = allJobData.filter(
+      (singleRemoteJob) => singleRemoteJob.employee_type === "Remote"
+    );
+    setAllJobData(remoteJob);
+    setIsOpen(!isOpen);
+  };
+  const handleFilterOffline = () => {
+    const offlineJob = allJobData.filter(
+      (singleOfflineJob) => singleOfflineJob.employee_type === "Office"
+    );
+    setAllJobData(offlineJob);
+    setIsOpen(!isOpen);
+  };
+  const handleFilterAll = () => {
+    setFilterShow(!filterShow);
+    setAllJobData(allJobData);
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
       <div className="title py-16">Applied jobs</div>
       <div className="my-container py-12">
+        {/* filter section starts here  */}
         <div className="flex justify-end">
-          <p>filter by</p>
+          <div className="relative ">
+            <div
+              onClick={() => setIsOpen(!isOpen)}
+              className="btn-primary w-28 cursor-pointer select-none p-2"
+            >
+              Filter by
+            </div>
+            {isOpen && (
+              <div className="absolute top-11 ">
+                <button className="btn-outline w-28" onClick={handleFilterAll}>
+                  All Data
+                </button>
+                <button
+                  className="btn-outline w-28"
+                  onClick={handleFilterRemote}
+                >
+                  Remote
+                </button>
+                <button
+                  className="btn-outline w-28"
+                  onClick={handleFilterOffline}
+                >
+                  Offline
+                </button>
+              </div>
+            )}
+          </div>
         </div>
-        <div className="p-12">
-          {jobDataArray &&
-            jobDataArray.map((appliedSingleJobData) => (
+        <div className="flex flex-col gap-6 px-32 py-16">
+          {allJobData &&
+            allJobData.map((appliedSingleJobData) => (
               <AppliedSingleJob
                 appliedSingleJobData={appliedSingleJobData}
                 key={appliedSingleJobData.job_id}
--- src/components/AppliedSingleJob.jsx
+++ src/components/AppliedSingleJob.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const AppliedSingleJob = () => {
-  return <div>singele job</div>;
+const AppliedSingleJob = ({ appliedSingleJobData }) => {
+  const {
+    company_name,
+    company_logo,
+    employee_type,
+    employment_type,
+    job_title,
+    job_id,
+    location,
+    salary_range,
+  } = appliedSingleJobData;
+  return (
+    <div className="flex items-center justify-between space-y-3 rounded-lg border border-gray-400 p-4 hover:bg-gray-50">
+      <div className="flex items-center gap-6">
+        <img
+          className="aspect-video h-full w-40 object-contain"
+          src={company_logo}
+          alt=""
+        />
+        <div className="space-y-2">
+          <h2 className="text-xl font-semibold text-gray-800">{job_title}</h2>
+          <p className="font-medium text-gray-700">{company_name}</p>
+          <div className="space-x-4">
+            <button className="btn-outline">{employee_type}</button>
+            <button className="btn-outline">{employment_type}</button>
+          </div>
+          <div className="flex gap-8 font-medium text-gray-700">
+            <p className="flex gap-2">
+              <img src="/public/assets/Icons/Location Icon.png" alt="" />{" "}
+              {location}
+            </p>
+            <p className="flex space-x-2 font-medium text-gray-600">
+              <img src="/public/assets/Icons/Frame.png" alt="" />
+              <span>Salary:</span>
+              <span>{salary_range}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* view details button  */}
+      <div>
+        <Link to={`/featureJob-details/${job_id}`} className="btn-primary py-2">
+          View Details
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default AppliedSingleJob;
